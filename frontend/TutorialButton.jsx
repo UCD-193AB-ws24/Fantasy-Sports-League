c53f2141654e50c021d176d87dc218526dce1c5c
Khanh Nguyen <116329829+khunguyen@users.noreply.github.com>
2025-02-18 18:28:33 -0800

    Add files via upload

---
+++ frontend/TutorialButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TutorialButton.css'
+
+const TutorialButton = () => {
+    return (
+        <a href="https://www.youtube.com/watch?v=5573VV86ju4" target="_blank" rel="noopener noreferrer">
+          <button className='button-28'>How to Play?</button>
+        </a>
+      );
+    };
+
+export default TutorialButton;
