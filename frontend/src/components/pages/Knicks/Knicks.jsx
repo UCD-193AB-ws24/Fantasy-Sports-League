364dc0445786b973b9c8580434e664df72c9bc74
Khanh Nguyen <116329829+khunguyen@users.noreply.github.com>
2025-02-23 00:05:09 -0800

    Added Knicks folder files

---
+++ frontend/src/components/pages/Knicks/Knicks.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Knicks= ({ onEventClick }) =>{
+    const events = [
+        {
+          date: "FEB 20 (PAST)",
+          description: "vs. Philadelphia 76ers",
+          images: ["boston2-20.png", "76ers2-20.png"] // Two images
+        },
+        {
+          date: "FEB 23",
+          description: "vs. New York Knicks",
+          images: ["bostonBet.png", "knicksBet.png"]
+        },
+        {
+          date: "FEB 25",
+          description: "vs. Toronto Raptors",
+          images: ["bostonBet.png", "raptorsBet.png"]
+        },
+        {
+          date: "FEB 26",
+          description: "vs. Detroit Pistons",
+          images: ["bostonBet.png", "pistonsBet.png"]
+        },
+        {
+          date: "FEB 28",
+          description: "vs. Cleveland Cavaliers",
+          images: ["bostonBet.png", "cavaliersBet.png"]
+        },
+        {
+          date: "MAR 2",
+          description: "vs. Denver Nuggets",
+          images: ["bostonBet.png", "nuggetsBet.png"]
+        },
+        {
+          date: "MAR 5",
+          description: "vs. Portland Blazers",
+          images: ["bostonBet.png", "blazersBet.png"]
+        }
+      ];
+    
+      return (
+        <div className="container">
+          <div className="knicks-wrapper">
+            <h1 className='knicks-h1'>New York Knicks Schedule</h1>
+            <ul className="sessions">
+              {events.map((event, index) => (
+                <li key={index} className="knicks-times" onClick={() => onEventClick(event)}>
+                  <div className="knicks-time">{event.date}</div>
+                  <p className='knicks-p'>{event.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    };
+
+export default Knicks
