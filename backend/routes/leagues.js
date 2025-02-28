61d04bb92938ac8ee44837762e5439cd39bb50cf
RPadhi678 <116604147+RPadhi678@users.noreply.github.com>
2025-02-27 19:18:06 -0800

    Update leagues.js w completed code

--- backend/routes/leagues.js
+++ backend/routes/leagues.js
@@ -1,1 +1,34 @@
+import express from "express";
+import pool from "../config/db.js";
 
+const router = express.Router();
+
+// Create a League
+router.post("/", async (req, res) => {
+  try {
+    const { name, commissioner_id } = req.body;
+
+    const newLeague = await pool.query(
+      "INSERT INTO leagues (name, commissioner_id) VALUES ($1, $2) RETURNING *",
+      [name, commissioner_id]
+    );
+
+    res.json(newLeague.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Get All Leagues
+router.get("/", async (req, res) => {
+  try {
+    const leagues = await pool.query("SELECT * FROM leagues");
+    res.json(leagues.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+export default router;
